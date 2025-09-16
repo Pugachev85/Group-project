602dc960e233ac69d35583ce4b2c95e8d4a590ed
Мария Рудомётова <mariarudometova@MacBook-Air-Maria.local>
2025-09-16 17:43:07 +0300

    create parser file

--- src/MenuActions/FillFromFile.java
+++ src/MenuActions/FillFromFile.java
@@ -1,8 +1,24 @@
 package MenuActions;
 
+import Entity.Person;
+import Utils.PersonParser;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
 public class FillFromFile implements MenuStrategy {
     @Override
     public void execute() {
-        // код парсера
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите путь к файлу (.txt или .csv): ");
+        String path = scanner.nextLine();
+
+        try {
+            List<Person> persons = PersonParser.parse(path);
+            System.out.println("Файл успешно загружен! Найдено " + persons.size() + " записей.");
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла: " + e.getMessage());
+        }
+        }
     }
-}
