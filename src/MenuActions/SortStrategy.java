e5191fd59d530f31f4de4bcf2c8f230c142f0348
Мария Рудомётова <mariarudometova@MacBook-Air-Maria.local>
2025-09-18 09:42:14 +0300

    add saving result to a file

--- src/MenuActions/SortStrategy.java
+++ src/MenuActions/SortStrategy.java
@@ -3,7 +3,7 @@ package MenuActions;
 import Entity.Person;
 import Sorting.QuickSort;
 import Sorting.SortAlgorithm;
-
+import Utils.FileSaver;
 import java.util.Comparator;
 
 public class SortStrategy implements MenuStrategy {
@@ -15,7 +15,10 @@ public class SortStrategy implements MenuStrategy {
 
     @Override
     public void execute() {
-        if (DataBase.personCollection.isEmpty()) System.out.println("Коллекция пока пуста. Сначала заполните ее!");
+        if (DataBase.personCollection.isEmpty()) {
+            System.out.println("Коллекция пока пуста. Сначала заполните ее!");
+            return;
+        }
 
         Comparator<Person> comparator;
 
@@ -31,5 +34,7 @@ public class SortStrategy implements MenuStrategy {
 
         System.out.println("Коллекция отсортирована! Результат:");
         DataBase.personCollection.forEach(System.out::println);
+
+        FileSaver.saveCollection(DataBase.personCollection);
     }
 }
