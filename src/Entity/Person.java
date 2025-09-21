c71dd8edf72d10f120e3eeae0ff056b512eace8a
Pugachev85 <workpost2009@mail.ru>
2025-09-22 00:07:24 +0300

    Add all fields Comparator nj Person

--- src/Entity/Person.java
+++ src/Entity/Person.java
@@ -1,5 +1,7 @@
 package Entity;
 
+import java.util.Comparator;
+
 public class Person {
     private final String name;
     private final String surname;
@@ -55,5 +57,19 @@ public class Person {
                 ", surname='" + surname + '\'' +
                 ", birthYear=" + birthYear +
                 '}';
+    }
+
+    public static Comparator<Person> compareByAllFields() {
+        return (p1, p2) -> {
+            int surnameCompare = p1.getSurname().compareTo(p2.getSurname());
+            if (surnameCompare != 0) {
+                return surnameCompare;
+            }
+            int nameCompare = p1.getName().compareTo(p2.getName());
+            if (nameCompare != 0) {
+                return nameCompare;
+            }
+            return Integer.compare(p1.getBirthYear(), p2.getBirthYear());
+        };
     }
 }
